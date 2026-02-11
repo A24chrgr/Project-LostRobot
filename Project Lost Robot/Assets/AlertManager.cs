c1ada7359b2423acbbfe9ee7547a820218744044
Isak-Johansson05 <d24isajo@student.his.se>
2026-02-11 17:05:25 +0100

    Fix: UI Animation

--- Project Lost Robot/Assets/AlertManager.cs
+++ Project Lost Robot/Assets/AlertManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 namespace Grupp14
 {
@@ -7,25 +8,109 @@ namespace Grupp14
     {
         [SerializeField] private GameObject alertBox;
         private RectTransform alertBoxRect;
+
         public float directiveAlertDuration = 5f;
         public float subDirectiveAlertDuration = 5f;
 
-        [SerializeField] private float alertYOffset;
-        
+        private Alert currentAlert;
+        public Alert CurrentAlert => currentAlert;
+
+        private bool fadeIn = false;
+        private bool fadeOut = false;
+
+        [SerializeField] private float alertAnimationSpeed = 1f;
+        [SerializeField] private float alertYOffset = 75f;
+        [SerializeField] private AnimationCurve easeCurve;
+        [SerializeField] private float fadeDuration = 0.5f;
+
+        private float fadeTimer = 0f;
+        private float startY;
+        private float targetY;
+
+        public UnityEvent onAlertStarted;
+        public UnityEvent onAlertEnded;
+
         public Queue<Alert> alertQueue = new Queue<Alert>();
-        
+
         void Start()
         {
             alertBoxRect = alertBox.GetComponent<RectTransform>();
             alertBoxRect.anchoredPosition = new Vector2(0, alertBoxRect.sizeDelta.y);
         }
-        
+
         void Update()
         {
-        
+            if (currentAlert == null)
+            {
+                TryStartAlert();
+            }
+            else
+            {
+                if (fadeIn)
+                {
+                    fadeTimer += Time.deltaTime;
+                    float progress = Mathf.Clamp01(fadeTimer / fadeDuration);
+                    float curved = easeCurve.Evaluate(progress);
+
+                    float newY = Mathf.Lerp(startY, targetY, curved);
+                    alertBoxRect.anchoredPosition = new Vector2(0, newY);
+
+                    if (progress >= 1f)
+                    {
+                        fadeIn = false;
+                    }
+                }
+                else if (fadeOut)
+                {
+                    fadeTimer += Time.deltaTime;
+                    float progress = Mathf.Clamp01(fadeTimer / fadeDuration);
+                    float curved = easeCurve.Evaluate(progress);
+
+                    float newY = Mathf.Lerp(startY, targetY * 2, curved);
+                    alertBoxRect.anchoredPosition = new Vector2(0, newY);
+
+                    if (progress >= 1f)
+                    {
+                        fadeOut = false;
+                        currentAlert = null;
+                        onAlertEnded?.Invoke();
+                    }
+                }
+                else
+                {
+                    currentAlert.duration -= Time.deltaTime;
+                    Debug.Log("Duration left: " + currentAlert.duration);
+
+                    if (currentAlert.duration <= 0)
+                    {
+                        fadeOut = true;
+                        fadeIn = false;
+                        fadeTimer = 0f;
+
+                        startY = alertBoxRect.anchoredPosition.y;
+                        targetY = alertBoxRect.sizeDelta.y;
+                    }
+                }
+            }
+        }
+
+        private void TryStartAlert()
+        {
+            if (alertQueue.TryDequeue(out Alert newAlert))
+            {
+                currentAlert = newAlert;
+                onAlertStarted?.Invoke();
+
+                fadeIn = true;
+                fadeOut = false;
+                fadeTimer = 0f;
+
+                startY = alertBoxRect.anchoredPosition.y;
+                targetY = -alertYOffset;
+            }
         }
     }
-    
+
     public class Alert
     {
         public string title, message;
